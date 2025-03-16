fa973676a49bf0db060bcab111d5dd621b1f2805
Ruslan Zhulii <39898339+dropson@users.noreply.github.comm>
2025-03-16 23:28:30 +0100

    feat: add roles withh Spatie and seeder

---
+++ app/Enums/UserRoleEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum UserRoleEnum : string
+{
+
+    case Admin = 'admin';
+    case Moderator = 'moderator';
+    case Coach = 'coach';
+    case Client = 'client';
+
+}
---
+++ database/seeders/RolesAndPermissionSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\UserRoleEnum;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $admin = Role::firstOrCreate(["name"=> UserRoleEnum::Admin->value]);
+
+        $moderator = Role::firstOrCreate(["name"=> UserRoleEnum::Moderator->value]);
+
+        $coach = Role::firstOrCreate(["name"=> UserRoleEnum::Coach->value]);
+
+        $client = Role::firstOrCreate(["name"=> UserRoleEnum::Client->value]);
+    }
+}
